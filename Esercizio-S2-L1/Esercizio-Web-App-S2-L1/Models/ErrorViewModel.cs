815d6c6a80d888e1b8af654f98564a36abd14d45
shauniif <francescavescio12@gmail.com>
2024-06-24 15:19:47 +0200

    rimosso Web-App

--- Esercizio-S2-L1/Esercizio-Web-App-S2-L1/Models/ErrorViewModel.cs
+++
@@ -1,9 +0,0 @@
-namespace Esercizio_Web_App_S2_L1.Models
-{
-    public class ErrorViewModel
-    {
-        public string? RequestId { get; set; }
-
-        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-    }
-}
